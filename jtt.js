628a630c7180bda99626d70b5738a7c44f9b96fb
Dariusz Pasciak <dadarek@sbcglobal.net>
2012-02-15 14:58:43 -0600

    checking in changes from January (http://www.youtube.com/watch?v=DoDNGRrd-nU)

--- jtt.js
+++ jtt.js
@@ -1,43 +1,72 @@
+var JTT = {
+    assert: function (expected, actual) {
+        if (expected !== actual)
+            throw new Error();
+    },
+    passes: 0,
+    fails: 0,
+    pass: function () {
+        this.passes++;
+    },
+    fail: function () {
+        this.fails++;
+    },
+    testPassFail: function () {
+        this.fail();
+        this.fail();
 
-var JTT = (function() {
-	var JTT,
-		isBrowser,
-		printErrorOnBrowser,
-		printErrorOnConsole,
-		printError,
-		assertTrue
-	;
-	
-	JTT = { };
-	isBrowser = !(typeof document === 'undefined');
-	
-	printErrorOnBrowser = function(message) {
-		document.write(message);
-	};
+        this.pass();
+        this.pass();
 
-	printErrorOnConsole = function(message) {
-		console.error(message);
-	};
-	
-	printError = function(message) {
-		if(isBrowser) {
-			printErrorOnBrowser(message);
-		}
-		else {
-			printErrorOnConsole(message);
-		}
-	};
-	
-	assertTrue = function(condition, messageIfFalse) {
-		if(!condition) {
-			printError(messageIfFalse);
-		}
-	};
-	
-	JTT.assertTrue = assertTrue;
-	
-	return JTT;
-}());
+        this.assert(2, this.fails);
+        this.assert(2, this.passes);
+    },
+    isTestFunction: function (element, obj) {
+        var beginsWithTest = element.substring(0, 4) === 'test';
+        var isFunction = typeof (obj[element]) === 'function';
+        return beginsWithTest && isFunction;
+    },
+    runTestSuite: function (testSuite) {
+        for (element in testSuite) {
+            if (this.isTestFunction(element, testSuite)) {
+                testSuite[element]();
+            }
+        }
+    },
+    run: function () {
+        this.testPassFail();
 
-JTT.assertTrue(1 === 3, "1 does not equal 3");
-JTT.assertTrue(1 === 1, "1 should equal 1");
+        var testARun = false;
+        var someFunctionRun = false;
+
+        var testSuite = {
+            someVar: null,
+            testVar: null,
+            testA: function () {
+                testARun = true;
+            },
+            someFunction: function () {
+                someFunctionRun = true;
+            }
+        };
+        this.runTestSuite(testSuite);
+        this.assert(true, testARun);
+        this.assert(false, someFunctionRun);
+
+    }
+};
+
+
+JTT.run();
+
+var myTestSuite = {
+    testFunction: function () {
+        JTT.assert(1, 1);
+    },
+    helperFunction: function () {
+        // should not run
+        JTT.assert(2, 1);
+    }
+};
+
+JTT.runTestSuite(myTestSuite);
